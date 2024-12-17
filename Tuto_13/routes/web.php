64eea7e269242687ca9000f2e83bcde107e52e38
Suirita Fahd <suirita.fahd.solicode@gmail.com>
2024-12-17 21:15:35 +0100

    Tuto_13

---
+++ Tuto_13/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+  return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/dashboard', function () {
+  return view('dashboard');
+})->middleware('auth');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
