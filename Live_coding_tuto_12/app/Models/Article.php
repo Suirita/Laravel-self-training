5896c63edced13e7075c39afa4d3140ce3e07b00
Suirita Fahd <suirita.fahd.solicode@gmail.com>
2024-12-20 21:23:40 +0100

    Prototype_4

---
+++ Live_coding_tuto_12/app/Models/Article.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    protected $fillable = [
+        'title',
+        'content',
+        'user_id',
+        'tags'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'article_tag');
+    }
+
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
+}
